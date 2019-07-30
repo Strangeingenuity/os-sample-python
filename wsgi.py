a12bb38f86e17ec48d83ab76f6b758ebf66db76d
Strangeingenuity <Strangeingenuity@users.noreply.github.com>
2019-07-29 19:22:14 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,12 @@
 from flask import Flask
 application = Flask(__name__)
+import requests
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    response = requests.get('https://httpbin.org/ip')
+    return 'Your IP is {0}'.format(response.json()['origin']))
+    
 
 if __name__ == "__main__":
     application.run()
