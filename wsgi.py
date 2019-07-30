d9be45653edbddffff14bf9949a22c158b8b3cbc
Strangeingenuity <Strangeingenuity@users.noreply.github.com>
2019-07-29 19:26:12 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ import requests
 @application.route("/")
 def hello():
     response = requests.get('https://httpbin.org/ip')
-    return 'Your IP is {0}'.format(response.json()['origin']))
+    return response
     
 
 if __name__ == "__main__":
